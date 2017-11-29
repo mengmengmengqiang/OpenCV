038bd1b554a2854058fffbe4fad6cf30484efc38
RQ <mengmengmengqiang@gmail.com>
2017-11-29 22:38:18 +0800

    修改书中关于画眼睛矩形框的错误

--- Face/test_video.py
+++ Face/test_video.py
@@ -33,7 +33,7 @@ def detect():
             eyes = eye_cascade.detectMultiScale(roi_gray, 1.03, 5, 0, (40, 40))
 
             for (ex,ey,ew,eh) in eyes:
-                cv2.rectangle(img, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+                cv2.rectangle(img, (x+ex, y+ey), (x+ex+ew, y+ey+eh), (0, 255, 0), 2)
                 cv2.imshow("camera", frame)
                 if cv2.waitKey(int(1000 / 12)) & 0xff == ord("q"):
                     break
